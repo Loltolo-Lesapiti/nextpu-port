d0a9cb1e3263c9975184572473e705d9b8ca8200
Petro Lesapiti <petrolesapiti@gmail.com>
2023-10-12 12:56:46 +0300

    Update styling

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function page() {
   return (
-    <section className="bg-white dark:bg-gray-900 antialiased">
+    <section className="bg-gray-200 dark:bg-gray-900 antialiased">
       <div className="max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-16 lg:py-24">
         <div className="max-w-2xl mx-auto text-center">
           <h2 className="text-3xl font-extrabold leading-tight tracking-tight text-gray-900 sm:text-4xl dark:text-white">
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const Page = () => {
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-white dark:bg-gray-700">
       <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div className="max-w-screen-md mb-8 lg:mb-16">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+          <h2 className="mb-4 lg:mt-8 md:mt-6 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
             Lorem ipsum dolor sit amet consectetur
           </h2>
           <p className="text-gray-500 sm:text-xl dark:text-gray-400">
