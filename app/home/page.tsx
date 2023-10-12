c42f356fcdae083ff0cd8f9441c1b030ae190830
Petro Lesapiti <petrolesapiti@gmail.com>
2023-10-12 12:55:45 +0300

    Update hero section.

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,11 +2,11 @@
 import React from "react";
 const Page = () => {
   return (
-    <section className="bg-white dark:bg-gray-900">
-      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+    <section className="bg-gray-200 dark:bg-gray-900">
+      <div className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-300 grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7 ">
-          <h1 className="page-title max-w-2xl mb-4 text-4xl font-bold text-#002d5b tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-            Hello, Welcome to my Space
+          <h1 className="page-title max-w-2xl mb-4 text-3xl font-semibold text-neutral-800 tracking-tight leading-none md:text-4xl xl:text-5xl dark:text-white">
+            Hello! I&apos;m Pure.
           </h1>
           <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
